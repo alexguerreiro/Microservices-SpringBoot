8a92ae482ce5413619c6fb6948b7027b423801b5
Mac <mac@Macs-Air.home>
2019-05-04 22:09:14 +0100

    adding more configs

--- limits-service/src/main/java/com/example/limitsservice/controller/LimitsConfigController.java
+++ limits-service/src/main/java/com/example/limitsservice/controller/LimitsConfigController.java
@@ -2,6 +2,7 @@ package com.example.limitsservice.controller;
 
 import com.example.limitsservice.Config;
 import com.example.limitsservice.beans.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,15 @@ public class LimitsConfigController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration(){
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfig")
+    public LimitConfiguration retrieveConfig() {
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfig() {
+        return new LimitConfiguration(9, 999);
     }
 }
